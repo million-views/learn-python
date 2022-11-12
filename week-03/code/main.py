1a8e50d534b6f8a4cb8a4878e65f3a27f2f712bd
Vijay Aswadhati <vijay@m5nv.com>
2022-11-12 07:56:13 -0800

    week-03: level 1 solution using library

--- week-03/code/main.py
+++ week-03/code/main.py
@@ -23,8 +23,41 @@ dbfilename = os.path.dirname(os.path.abspath(__file__)) + '/transactions.csv'
 # the objective of this assignment is to get you comfortable in using
 # lists, tuples, sets and dicts while studying a useful application...
 # so don't worry if the results are not `magical`
-min_support = 0.1
+min_support = 0.15
 min_confidence = 0.5
+
+#      a  b  c d e f
+# I => 🍎 🍺 🍚 🍗 🍐 🍼
+emoji_t1_set = [
+    ('🍎', '🍺', '🍚', '🍗'),  # tx 1
+    ('🍎', '🍺', '🍚'),        # tx 2
+    ('🍎', '🍺'),              # tx 3
+    ('🍎', '🍐'),              # tx 4
+    ('🍼', '🍺', '🍚', '🍗'),   # tx 5
+    ('🍼', '🍺', '🍚'),       # tx 6
+    ('🍼', '🍺'),             # tx 7
+    ('🍼', '🍐'),             # tx 8
+]
+
+#      a  b  c d  e
+# I => 🥚 🥓 🍲 🍎 🍌
+emoji_t2_set = [
+    ('🥚', '🥓', '🍲'),  # eggs, bacon, soup
+    ('🥚', '🥓', '🍎'),  # eggs, bacon, apple
+    ('🍲', '🥓', '🍌')  # soup, bacon, banana
+]
+# [{eggs} -> {bacon}, {soup} -> {bacon}] @ min_support=0.5, min_confidence=1
+
+#      a  b  c d  e  f
+# I => 🍞 🥛 👶 🍺 🥚 🥤
+emoji_t3_set = [
+  ('🍞', '🥛'),              # tx 1
+  ('🍞', '👶', '🍺', '🥚'),   # tx 2
+  ('🥛', '👶', '🍺', '🥤'),   # tx 3
+  ('🍞', '🥛', '👶', '🍺'),   # tx 4
+  ('🍞', '🥛', '👶', '🥤'),   # tx 5
+]
+
 try:
     transactions = []
     with open(dbfilename) as db:
@@ -38,11 +71,14 @@ try:
             # print(len(basket), basket)
 
     # call the `analyze` function that does `apriori` on `transactions`
-    itemsets, rules = papack.analyze(
-        transactions,
-        min_support,
-        min_confidence,
-    )
-    print(itemsets, rules)
+    # transactions = emoji_t2_set
+    itemsets, rules = papack.analyze(transactions, min_support, min_confidence,
+                                     True)
+    # print(itemsets, rules)
+    # print(rules)
+    for rule in rules:
+      print(rule)
+
 except Exception as e:
     print('Oops!', e)
+    # raise e
--- week-03/code/papack.py
+++ week-03/code/papack.py
@@ -4,18 +4,11 @@ PaPack: contains super helpful and fast predictive analytics functions
 A pretend module that exists to illustrate code organization and to
 isolate class assignment instructions in one place.
 """
-
+from efficient_apriori import apriori
 
 def analyze(transactions, min_support=0.5, min_confidence=1, uselib=True):
-    """
-    assignment (level 1, make code changes in week-3/code/papack.py):
-      - import `apriori` function from `efficient_apriori` library
-      - study the `efficient_apriori` library doc,
-      - call the `apriori` function with arguments received in this function
-      - return the results as a tuple
-    assignment (level 2):
-      - if uselib is False, invoke your own `apriori` implementation
-        and return the results as a tuple.
-    """
-    print(analyze.__doc__)
-    return ("good", "luck!")
+    if uselib is True:
+      itemsets, rules = apriori(transactions, min_support, min_confidence)
+      return (itemsets, rules)
+    else:
+      return ("I am ", "trying")
